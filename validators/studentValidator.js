6c3f7efab0c688ec66eee2f129b69fd1dc08167c
akshay30rwt <akshayrwt8700@gmail.com>
2026-07-04 20:15:53 +0530

    fix: decapitalize String and Number

--- validators/studentValidator.js
+++ validators/studentValidator.js
@@ -1,9 +1,9 @@
 const Joi = require('joi');
 
 const studentSchema = Joi.object({
-    name: Joi.String().min(2).max(50).required(),
-    age: Joi.Number().min(5).max(100).required(),
-    grade: Joi.String().min(1).max(2).required()
+    name: Joi.string().min(2).max(50).required(),
+    age: Joi.number().min(5).max(100).required(),
+    grade: Joi.string().min(1).max(2).required()
 });
 
 module.exports = studentSchema;
