39afd0dd2acf0a28cba7f3aeecf0a18c7f58b2ad
akshay30rwt <akshayrwt8700@gmail.com>
2026-07-07 18:40:07 +0530

    feat: add pagination in getAllStudents

--- index.js
+++ index.js
@@ -37,8 +37,20 @@ app.get('/', (req, res) => {
 
 app.get('/students', async (req, res, next) => {
     try {
-        const students = await Student.find();
-        res.status(200).json(students);
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
+
+        const students = await Student.find().skip(skip).limit(limit);
+
+        const totalDocs = await Student.countDocuments();
+
+        return res.status(200).json({
+            totalDocs,
+            page,
+            totalPages: Math.ceil(totalDocs/limit),
+            data: students
+        });
     }
     catch(err) {
         next(err);
